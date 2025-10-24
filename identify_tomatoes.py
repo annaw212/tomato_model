154efc5d0dfdf237dff7e45c6c6710e7decf853b
Anna Elise Wong <annaelisewong@Annas-MacBook-Pro.local>
2025-10-23 22:34:19 -0700

    Add in a git ignore

---
+++ identify_tomatoes.py
@@ -0,0 +1,13 @@
+# Do something here
+
+# Load the model
+# use it
+# don't die
+
+from argparse import ArgumentParser
+
+
+
+# import photo foilname as text input
+# determine whether that is tomato
+# print yes or no
